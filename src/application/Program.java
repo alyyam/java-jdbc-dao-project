7d0c3c670c7e2e3bf8e9c0193074a5121d24b95c
Alysson Machado <alysson-am@hotmail.com>
2022-03-16 16:05:00 +1100

    DepartmentDao and SellerDao interfaces

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,7 +9,7 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Department  dep = new Department(1, "Books");
+		Department  dep = new Department(1, "  Books");
 		Seller seller = new Seller(21, "Alysson", "alysson-am@gmail.com", new Date(), 2000.00, dep);
 		
 		System.out.println(seller);
