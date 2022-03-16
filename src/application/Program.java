bd09a7015079c09c63654755d410568a9b21e95c
Alysson Machado <alysson-am@hotmail.com>
2022-03-16 15:53:26 +1100

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Department dp = new Department(1, "books");
 		
-		System.out.println(dp);
+		Department  dep = new Department(1, "Books");
+		Seller seller = new Seller(21, "Alysson", "alysson-am@gmail.com", new Date(), 2000.00, dep);
+		
+		System.out.println(seller);
 	}
 
 }
