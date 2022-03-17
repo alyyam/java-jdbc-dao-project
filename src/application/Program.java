f43d696127b85a5490594518ea1839c6bec14332
Alysson Machado <alysson-am@hotmail.com>
2022-03-17 17:10:09 +1100

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -41,6 +41,15 @@ public class Program {
 		Seller newSeller = new Seller(null, "Greg", "greag@gmail.com", new Date(), 4000.00, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New Id = " + newSeller.getId()); 
+		
+		
+		System.out.println("\n=== TEST 5: Update Seller===");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.udpate(seller);
+		System.out.println("update completed");
 	}
+	
+	
 
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -33,18 +33,18 @@ public class SellerDaoJDBC implements SellerDao{
 			st = conn.prepareStatement(
 
 					"INSERT INTO seller "
-					+"(Name, Email, BirthDate, BaseSalary, DepartmentId) "
-					+ "VALUES (?, ?, ?, ?, ?) ", Statement.RETURN_GENERATED_KEYS);
-			
+							+"(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+							+ "VALUES (?, ?, ?, ?, ?) ", Statement.RETURN_GENERATED_KEYS);
+
 			st.setString(1, obj.getName());
 			st.setString(2, obj.getEmail());
 			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
 			st.setDouble(4, obj.getBaseSalary());
 			st.setInt(5, obj.getDeparment().getId());
-			
-			
+
+
 			int rowsAffected = st.executeUpdate();
-			
+
 			if(rowsAffected > 0) {
 				ResultSet rs = st.getGeneratedKeys();
 				if(rs.next()) {
@@ -54,7 +54,7 @@ public class SellerDaoJDBC implements SellerDao{
 				DB.closeResultSet(rs);
 			}
 			else {
-				
+
 				throw new DbException("Unexpected error, no rows affected");
 			}
 		}
@@ -63,14 +63,38 @@ public class SellerDaoJDBC implements SellerDao{
 		}
 		finally {
 			DB.closeStatement(st);
-			
+
 		}
 
 	}
 
 	@Override
 	public void udpate(Seller obj) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+
+		try {
+			st = conn.prepareStatement(
+					"UPDATE seller "
+					+"SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? "
+					+"WHERE Id = ?" );
+
+			st.setString(1, obj.getName());
+			st.setString(2, obj.getEmail());
+			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+			st.setDouble(4, obj.getBaseSalary());
+			st.setInt(5, obj.getDeparment().getId());
+			st.setInt(6, obj.getId());
+
+			st.executeUpdate();
+
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+
+		}
 
 	}
 
