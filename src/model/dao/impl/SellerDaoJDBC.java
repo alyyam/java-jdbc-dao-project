3500933b32ad1c0588c8e60c5790457dce0ef0f2
Alysson Machado <alysson-am@hotmail.com>
2022-03-17 16:25:24 +1100

    insert implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,10 +4,12 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 
 import db.DB;
 import db.DbException;
@@ -25,7 +27,44 @@ public class SellerDaoJDBC implements SellerDao{
 
 	@Override
 	public void insert(Seller obj) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+
+		try {
+			st = conn.prepareStatement(
+
+					"INSERT INTO seller "
+					+"(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+					+ "VALUES (?, ?, ?, ?, ?) ", Statement.RETURN_GENERATED_KEYS);
+			
+			st.setString(1, obj.getName());
+			st.setString(2, obj.getEmail());
+			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+			st.setDouble(4, obj.getBaseSalary());
+			st.setInt(5, obj.getDeparment().getId());
+			
+			
+			int rowsAffected = st.executeUpdate();
+			
+			if(rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				if(rs.next()) {
+					int id = rs.getInt(1);
+					obj.setId(id);
+				}
+				DB.closeResultSet(rs);
+			}
+			else {
+				
+				throw new DbException("Unexpected error, no rows affected");
+			}
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			
+		}
 
 	}
 
@@ -48,17 +87,17 @@ public class SellerDaoJDBC implements SellerDao{
 
 		try {
 			st = conn.prepareStatement(
-						"SELECT seller.*,department.Name as DepName "
+					"SELECT seller.*,department.Name as DepName "
 							+ "FROM seller INNER JOIN department "
 							+ "ON seller.DepartmentId = department.Id "
 							+ "WHERE seller.Id = ? ");
-			
+
 			st.setInt(1, id);
 			rs = st.executeQuery();
 			if(rs.next()) { // se o valor for encontrado o departamento precisa seer instanciado
 				Department dep = instantiateDep(rs);
 				Seller obj = instantiateSeller(rs, dep);
-				
+
 				return obj;
 			}
 			return null;
@@ -67,11 +106,11 @@ public class SellerDaoJDBC implements SellerDao{
 		catch(SQLException e) {
 			throw new DbException(e.getMessage());
 		}
-		
+
 		finally {
 			DB.closeStatement(st);
 			DB.closeResultSet(rs);
-		
+
 		}
 
 	}
@@ -96,48 +135,49 @@ public class SellerDaoJDBC implements SellerDao{
 
 	@Override
 	public List<Seller> findAll() {
+
 		PreparedStatement st = null;
 		ResultSet rs = null;
 
 		try {
 			st = conn.prepareStatement(
 					"SELECT seller.*,department.Name as DepName "
-					+ "FROM seller INNER JOIN department "
-					+ "ON seller.DepartmentId = department.Id "
-					
+							+ "FROM seller INNER JOIN department "
+							+ "ON seller.DepartmentId = department.Id "
+
 					+ "ORDER BY Name");
-			
-			
+
+
 			rs = st.executeQuery();
-			
+
 			List<Seller> list = new ArrayList<>();
 			Map<Integer,Department> map =  new HashMap<>(); // realiza o controle para que nao nao seja instanciado 2 departementos
-			
+
 			while(rs.next()) { // se o valor for encontrado o departamento precisa seer instanciado
-				
+
 				Department dep = map.get(rs.getInt("DepartmentId")); // busca dentro do map se ja existe algum departemento com ID passado
 				if(dep ==  null) { // significa que meu departmento nao existia e precisa ser instaciado
 					dep = instantiateDep(rs); //instancia o departamento
 					map.put(rs.getInt("DepartmentId"), dep); // coloca a key dentro do meu map para que o departamento nao seja instanciado novamente
-					
-					
+
+
 				}
-				
+
 				Seller obj = instantiateSeller(rs, dep);
 				list.add(obj);
-				
-				
+
+
 			}
 			return list;
 		}
 		catch(SQLException e) {
 			throw new DbException(e.getMessage());
 		}
-		
+
 		finally {
 			DB.closeStatement(st);
 			DB.closeResultSet(rs);
-		
+
 		}
 	}
 
@@ -149,42 +189,42 @@ public class SellerDaoJDBC implements SellerDao{
 		try {
 			st = conn.prepareStatement(
 					"SELECT seller.*,department.Name as DepName "
-					+ "FROM seller INNER JOIN department "
-					+ "ON seller.DepartmentId = department.Id "
-					+ "WHERE DepartmentId = ? "
-					+ "ORDER BY Name");
-			
+							+ "FROM seller INNER JOIN department "
+							+ "ON seller.DepartmentId = department.Id "
+							+ "WHERE DepartmentId = ? "
+							+ "ORDER BY Name");
+
 			st.setInt(1, department.getId());
 			rs = st.executeQuery();
-			
+
 			List<Seller> list = new ArrayList<>();
 			Map<Integer,Department> map =  new HashMap<>(); // realiza o controle para que nao nao seja instanciado 2 departementos
-			
+
 			while(rs.next()) { // se o valor for encontrado o departamento precisa seer instanciado
-				
+
 				Department dep = map.get(rs.getInt("DepartmentId")); // busca dentro do map se ja existe algum departemento com ID passado
 				if(dep ==  null) { // significa que meu departmento nao existia e precisa ser instaciado
 					dep = instantiateDep(rs); //instancia o departamento
 					map.put(rs.getInt("DepartmentId"), dep); // coloca a key dentro do meu map para que o departamento nao seja instanciado novamente
-					
-					
+
+
 				}
-				
+
 				Seller obj = instantiateSeller(rs, dep);
 				list.add(obj);
-				
-				
+
+
 			}
 			return list;
 		}
 		catch(SQLException e) {
 			throw new DbException(e.getMessage());
 		}
-		
+
 		finally {
 			DB.closeStatement(st);
 			DB.closeResultSet(rs);
-		
+
 		}
 	}
 
